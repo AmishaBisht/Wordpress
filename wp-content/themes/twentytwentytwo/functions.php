fc64534fb35e6ca40d4948e8add492747f730804
amisha bisht <amishabisht33@gmail.comgit status>
2022-10-11 15:20:23 +0530

    created taxonomy

--- wp-content/themes/twentytwentytwo/functions.php
+++ wp-content/themes/twentytwentytwo/functions.php
@@ -120,3 +120,29 @@ function hfm_register_custom_post_type() {
 
 }
 add_action( 'init', 'hfm_register_custom_post_type' );
+
+//Add custom taxonomies
+function add_custom_taxonomies() {
+	register_taxonomy('location', 'resource', array(
+	  'hierarchical' => true,
+	  'labels' => array(
+		'name' => _x( 'Locations', 'taxonomy general name' ),
+		'singular_name' => _x( 'Location', 'taxonomy singular name' ),
+		'search_items' =>  __( 'Search Locations' ),
+		'all_items' => __( 'All Locations' ),
+		'parent_item' => __( 'Parent Location' ),
+		'parent_item_colon' => __( 'Parent Location:' ),
+		'edit_item' => __( 'Edit Location' ),
+		'update_item' => __( 'Update Location' ),
+		'add_new_item' => __( 'Add New Location' ),
+		'new_item_name' => __( 'New Location Name' ),
+		'menu_name' => __( 'Locations' ),
+	  ),
+	  'rewrite' => array(
+		'slug' => 'locations', // This controls the base slug that will display before each term
+		'with_front' => false, // Don't display the category base before "/locations/"
+		'hierarchical' => true // This will allow URL's like "/locations/boston/cambridge/"
+	  ),
+	));
+  }
+  add_action( 'init', 'add_custom_taxonomies', 0 );
